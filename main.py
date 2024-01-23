9d230650e5f513d272d0a96dfc697e3dfc042532
Mindaugas Minelga <minelga.m@gmail.com>
2024-01-23 15:20:10 +0200

    modify main.py file add items.csv file

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-hello
+import csv
+
+
+class Item:
+    pay_rate = 0.8
+    all = []
+
+    def __init__(self, name: str, price: float, quantity=0) -> None:
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+    def calculate_total_price(self) -> float:
+        return self.price * self.quantity
+
+    def apply_discount(self) -> float:
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("items.csv", "r") as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get("name"),
+                price=float(item.get("price")),
+                quantity=int(item.get("quantity")),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self) -> str:
+        return (
+            f"Item (name='{self.name}', price={self.price}, quantity={self.quantity})"
+        )
+
+
+# item1 = Item(name="Phone", price=100, quantity=1)
+# item2 = Item(name="Laptop", price=1000, quantity=3)
+# item3 = Item(name="Cable", price=10, quantity=5)
+# item4 = Item(name="Mouse", price=50, quantity=5)
+# item5 = Item(name="Keyboard", price=75, quantity=6)
+
+print(Item.is_integer(10.0))
